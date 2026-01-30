1ebee6f63159f368d420c883d36c130f45b53c01
luigimnegri <luigi2negri@gmail.com>
2026-01-30 14:27:30 +0100

    Update inventario.js

--- js/inventario.js
+++ js/inventario.js
@@ -1,4 +1,4 @@
-const INVENTORY_API_BASE = 'https://cocktails-book-back-end.onrender.com/api/inventory'; // cambia se diverso
+const INVENTORY_API_BASE = 'https://cocktails-book-back-end.onrender.com/'; // cambia se diverso
 
 async function loadInventory() {
   try {
